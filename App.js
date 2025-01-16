a11b3feb04cac5cc92a9583a79b12b36118406c1
Awes Khan Ayyub Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-16 20:03:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import './../styles/App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+
+          <Routes>
+
+            <Route path="/" element={<Home />}/>
+            <Route path="/about" element={<About />}/>
+          
+          </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
