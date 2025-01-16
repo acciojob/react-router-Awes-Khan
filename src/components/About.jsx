3fed1f70656ee53a9ef51813f1401e1423c50b7f
Awes Khan Ayyub Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-16 19:48:38 +0530

    Add files via upload

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,15 @@
 
 import React from "react";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
-    <div>
+      <div>       
+          <ul>
+            <Link to={"/"}><li>Home</li></Link>
+            <Link to={"/about"}><li>About</li></Link>
+          </ul>
+          
           <h1>About</h1>
           <p>This is the about page. It provides information about the website.</p>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,10 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div>          
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-            </ul>
-        </nav>          
+        <ul>
+            <Link to={"/"}><li>Home</li></Link>
+            <Link to={"/about"}><li>About</li></Link>
+        </ul>      
       <h1>Welcome to the Home Page</h1>
     </div>
   )
