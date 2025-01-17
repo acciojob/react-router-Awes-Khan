e18de0e5ce4836c373198d1a4d682b0995e089d5
Awes Khan Ayyub Khan <44608315+Awes-Khan@users.noreply.github.com>
2025-01-17 14:55:01 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <div>          
+    <>          
         <ul>
             <Link to={"/"}><li>Home</li></Link>
             <Link to={"/about"}><li>About</li></Link>
         </ul>      
       <h1>Welcome to the Home Page</h1>
-    </div>
+    </>
   )
 }
 
